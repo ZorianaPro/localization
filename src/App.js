baca120c6605037605604a8d8451c576cda639b8
Zoriana <zoryana.lesyk@experteer.com>
2020-01-08 16:06:08 +0100

    Try i18next

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Suspense } from 'react';
 import './App.css';
-import translations from './support/localization';
+import L18nextComponent from "./components/I18next";
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        {translations.t("hello")}
+        <L18nextComponent/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as serviceWorker from  './registerServiceWorker';
+import {IntlProvider} from 'react-intl';
+// import Translation from './support/react-intl-init'
+
+import './i18n';
 
 ReactDOM.render(
-  <App />,
+		<App/>,
   document.getElementById('root')
 );
+serviceWorker.register();
